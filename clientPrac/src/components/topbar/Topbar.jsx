b6d8cb4a060d15b12a72c262e59e93c2c5d0efaf
iCode_Rx <arababs2015@gmail.com>
2021-08-10 12:21:13 +0100

    first commit

---
+++ clientPrac/src/components/topbar/Topbar.jsx
@@ -0,0 +1,10 @@
+import "./topbar.css"
+
+export default function Topbar() {
+
+          return (
+                    <div className="topbarContainer">
+                              Amen Halleluyah
+                    </div>
+          )
+}
---
+++ clientPrac/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.css"
+import Topbar from "../../components/topbar/Topbar"
+
+
+export default function Home() {
+
+          return (
+                    <div>
+                              <Topbar />
+                    </div>
+          )
+}
