e5a7403fedf6483d3d70bf34ae059776965d3e54
iCode_Rx <arababs2015@gmail.com>
2021-08-10 21:40:38 +0100

    leftbar updated

--- clientPrac/src/components/homeCom/leftbar/Leftbar.jsx
+++ clientPrac/src/components/homeCom/leftbar/Leftbar.jsx
@@ -1,10 +1,75 @@
 import "./leftbar.css"
+import { RssFeed, Chat, PlayCircleFilled, Group, Bookmark, HelpOutline, WorkOutline, Event, School } from "@material-ui/icons"
 
 export default function Leftbar() {
 
           return (
                     <div className="leftbarContainer">
-                              Leftbar
+
+                              <div className="leftbarWrapper">
+
+                                        <ul className="leftbarList">
+
+                                                  <li className="leftbarListItem">
+                                                            <RssFeed className="leftbarIcon" />
+                                                            <span className="leftbarItemText">Feed</span>
+                                                  </li>
+
+                                                  <li className="leftbarListItem">
+                                                            <Chat className="leftbarIcon" />
+                                                            <span className="leftbarItemText">Chats</span>
+                                                  </li>
+
+                                                  <li className="leftbarListItem">
+                                                            <PlayCircleFilled className="leftbarIcon" />
+                                                            <span className="leftbarItemText">Video</span>
+                                                  </li>
+
+                                                  <li className="leftbarListItem">
+                                                            <Group className="leftbarIcon" />
+                                                            <span className="leftbarItemText">Groups</span>
+                                                  </li>
+
+                                                  <li className="leftbarListItem">
+                                                            <Bookmark className="leftbarIcon" />
+                                                            <span className="leftbarItemText">Bookmarks</span>
+                                                  </li>
+
+                                                  <li className="leftbarListItem">
+                                                            <HelpOutline className="leftbarIcon" />
+                                                            <span className="leftbarItemText">Questions</span>
+                                                  </li>
+
+                                                  <li className="leftbarListItem">
+                                                            <WorkOutline className="leftbarIcon" />
+                                                            <span className="leftbarItemText">Jobs</span>
+                                                  </li>
+
+                                                  <li className="leftbarListItem">
+                                                            <Event className="leftbarIcon" />
+                                                            <span className="leftbarItemText">Events</span>
+                                                  </li>
+
+                                                  <li className="leftbarListItem">
+                                                            <School className="leftbarIcon" />
+                                                            <span className="leftbarItemText">Courses</span>
+                                                  </li>
+
+                                        </ul>
+
+                                        <button className="leftbarButton">Show More</button>
+
+                                        <hr className="leftbarHr" />
+
+                                        <ul className="leftbarFriendList">
+                                                  <li> Tayo </li>
+
+                                                  <li> Taye </li>
+
+                                                  <li> Tosin </li>
+                                        </ul>
+                              </div>
+
                     </div>
           )
 }
