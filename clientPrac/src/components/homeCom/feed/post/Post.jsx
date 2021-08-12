25cb467a5d4fce201e70504beaa24b4f977c0859
iCode_Rx <arababs2015@gmail.com>
2021-08-12 21:55:31 +0100

    profile pg created

--- clientPrac/src/components/homeCom/feed/post/Post.jsx
+++ clientPrac/src/components/homeCom/feed/post/Post.jsx
@@ -1,8 +1,16 @@
 import "./post.css"
 import { MoreVert } from "@material-ui/icons"
 import { Users } from "../../../../dummyApi"
+import { useState } from "react"
 
 export default function Post({ post }) {
+          const [like, setLike] = useState(post.like)
+          const [isLike, setIsLike] = useState(false)
+
+          const likeHandle = () => {
+                    setLike(isLike ? like - 1 : like + 1)
+                    setIsLike(!isLike)
+          }
 
           return (
                     <div className="post">
@@ -36,9 +44,19 @@ export default function Post({ post }) {
 
                                         <div className="postBottom">
                                                   <div className="postBottomLeft">
-                                                            <img src="/assets/like.png" alt="" className="likeIcon" />
-                                                            <img src="/assets/heart.png" alt="" className="likeIcon" />
-                                                            <span className="postLikeCounter">32 likes</span>
+                                                            <img 
+                                                                      src="/assets/like.png" 
+                                                                      alt="" 
+                                                                      className="likeIcon" 
+                                                                      onClick={likeHandle}          
+                                                            />
+                                                            <img 
+                                                                      src="/assets/heart.png" 
+                                                                      alt="" 
+                                                                      className="likeIcon" 
+                                                                      onClick={likeHandle}          
+                                                            />
+                                                            <span className="postLikeCounter">{like} likes</span>
                                                   </div>
 
                                                   <div className="postBottomRight">
