52449db50d7bdc9b5a2b8a060f186be1220d5272
iCode_Rx <arababs2015@gmail.com>
2021-08-14 14:25:31 +0100

    login created

--- clientPrac/src/components/homeCom/rightbar/Rightbar.jsx
+++ clientPrac/src/components/homeCom/rightbar/Rightbar.jsx
@@ -1,35 +1,35 @@
 import "./rightbar.css"
-import { Users } from "../../../dummyApi"
-import Online from "../rightbar/online/Online"
+// import { Users } from "../../../dummyApi"
+// import Online from "../rightbar/online/Online"
 
 export default function Rightbar( { profile }) {
 
-          const HomeRightbar = () => {
+          // const HomeRightbar = () => {
 
-                    return (
-                              <>
-                                        <div className="birthdayContainer">
-                                                  <img src="assets/gift.png" alt="" className="birthdayImg" />
+          //           return (
+          //                     <>
+          //                               <div className="birthdayContainer">
+          //                                         <img src="assets/gift.png" alt="" className="birthdayImg" />
 
-                                                  <span className="birthdayText">
-                                                            <b>Enaibe Henrie</b> and <b>3 other friends</b> have a birthday today
-                                                  </span>
-                                        </div>
+          //                                         <span className="birthdayText">
+          //                                                   <b>Enaibe Henrie</b> and <b>3 other friends</b> have a birthday today
+          //                                         </span>
+          //                               </div>
 
-                                        <img src="assets/ad.png" alt="" className="rightbarAd" />
+          //                               <img src="assets/ad.png" alt="" className="rightbarAd" />
 
-                                        <h4 className="rightbarTitle">Online Friends:</h4>
+          //                               <h4 className="rightbarTitle">Online Friends:</h4>
                                         
-                                        <ul className="rightbarFriendList">
-                                                  {
-                                                            Users.map(user => (
-                                                                      <Online  user ={user} key={user.id} />
-                                                            ))
-                                                  }
-                                        </ul>
-                              </>
-                    )
-          }
+          //                               <ul className="rightbarFriendList">
+          //                                         {
+          //                                                   Users.map(user => (
+          //                                                             <Online  user ={user} key={user.id} />
+          //                                                   ))
+          //                                         }
+          //                               </ul>
+          //                     </>
+          //           )
+          // }
 
           const ProfileRightbar = () => {
 
@@ -49,10 +49,44 @@ export default function Rightbar( { profile }) {
                                                   </div>
 
                                                   <div className="rightbarInfoItem">
-                                                            <span className="rightbarInfoKey">Relationship: </span>
+                                                            <span className="rightbarInfoKey">Relationship:</span>
                                                             <span className="rightbarInfoValue">Married</span>
                                                   </div>
 
+                                        </div>
+
+                                        <h4 className="rightbarTitle">User Friends: </h4>
+
+                                        <div className="rightbarFollowings">
+                                                  <div className="rightbarFollowing">
+                                                            <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                                                            <span className="rightbarFollowingName">John Doe</span>
+                                                  </div>
+
+                                                  <div className="rightbarFollowing">
+                                                            <img src="assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+                                                            <span className="rightbarFollowingName">John Doe</span>
+                                                  </div>
+
+                                                  <div className="rightbarFollowing">
+                                                            <img src="assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+                                                            <span className="rightbarFollowingName">John Doe</span>
+                                                  </div>
+
+                                                  <div className="rightbarFollowing">
+                                                            <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+                                                            <span className="rightbarFollowingName">John Doe</span>
+                                                  </div>
+
+                                                  <div className="rightbarFollowing">
+                                                            <img src="assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+                                                            <span className="rightbarFollowingName">John Doe</span>
+                                                  </div>
+
+                                                  <div className="rightbarFollowing">
+                                                            <img src="assets/person/6.jpeg" alt="" className="rightbarFollowingImg" />
+                                                            <span className="rightbarFollowingName">John Doe</span>
+                                                  </div>
                                         </div>
                               </>
                     )
