1076894d8becbf5e0d4e896858e0f35aedbb8db2
iCode_Rx <arababs2015@gmail.com>
2021-08-18 22:56:28 +0100

    more progress

--- rest-api/routes/auth.js
+++ rest-api/routes/auth.js
@@ -1,25 +1,54 @@
 const router = require("express").Router()
 const User = require("../models/User")
+const bcrypt = require("bcrypt")
 
 // register
 router.post("/register", async (req, res) => {
+          const salt = await bcrypt.genSalt(10)
+          const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
           const newUser = await new User(
                     {
                               username: req.body.username,
                               email: req.body.email,
-                              password: req.body.password
+                              // password: req.body.password
+                              password: hashedPassword
                     }
           )
 
           try {
                     const user = await newUser.save()
 
-                    res.send(200).json(user)
+                    res.status(200).json(user)
           } catch (err) {
-                    console.log(err)
+                    res.status(500).json(err)
           }
 
 
 })
 
+// login 
+router.post("/login", async(req, res) => {
+
+          try {
+                    // find user
+                    const user = await User.findOne(
+                              {
+                                        email: req.body.email
+                              }
+                    )
+                    !user && res.status(404).json("User Not Found")
+                    
+                    // find password
+                    const validPassword = await bcrypt.compare(req.body.password, user.password)
+                    !validPassword && res.status(400).json("Wrong Password")
+
+                    // correct email & password
+                    res.status(200).json(user)
+          } catch (err) {
+                    res.status(500).json(err)
+          }
+})
+
 module.exports = router;
+
--- rest-api/routes/user.js
+++ rest-api/routes/user.js
@@ -1,7 +1,44 @@
 const router = require("express").Router()
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
 
-router.get("/", (req, res) => {
-          res.send("Welcome to user route")
+// update user
+router.put("/:id", async (req, res)=> {
+          /*{
+                    "desc": "My first update",
+                    "userId": "611d7c719ce3481769f7d5c6"
+                    "_id": "611d7c719ce3481769f7d5c6"
+          }*/ 
+         
+          if(req.body.userId === req.params.id || req.user.isAdmin) {
+                    if(req.body.password) {
+                              try {
+                                        const salt = await bcrypt.genSalt(10)
+                                        req.body.password = await bcrypt.hash(req.body.password, salt)
+
+                              } catch (err) {
+                                        res.status(500).json(err)
+                              }
+
+                    }
+
+                    try {
+                              const user = await User.findByIdAndUpdate(req.params.id, {
+                                        $set: req.body
+                              })
+
+                              res.status(200).json("Account has been updated")
+                    } catch (err) {
+                              res.status(500).json(err)
+                    }
+
+          }else {
+                    res.status(403).json("You can only update your account")
+          }
 })
+// delete user
+// get a user
+// follow a user
+// unfollow a user
 
 module.exports = router;
