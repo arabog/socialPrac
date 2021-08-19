0294c9d97b132f4799cf248c3852092c710ddb79
iCode_Rx <arababs2015@gmail.com>
2021-08-19 12:32:36 +0100

    rest-api cmpleted

--- rest-api/routes/posts.js
+++ rest-api/routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 const Post = require("../models/Post")
+const User = require("../models/User")
 
 
 // cr8 post
@@ -60,13 +61,78 @@ router.delete("/:id", async (req, res) => {
 })
 
 
-// like a post
+// like/unlike a post
+router.put("/:id/like", async (req, res) => {
+          try {
+                    const post = await Post.findById(req.params.id)
 
+                    if(!post.likes.includes(req.body.userId)) {
+                              await post.updateOne(
+                                        {
+                                                  $push: {
+                                                            likes: req.body.userId
+                                                  }
+                                        }
+                              )
+
+                              res.status(200).json("The Post Has Been Liked")
+                    }else {
+                              await post.updateOne(
+                                        {
+                                                  $pull: {
+                                                            likes: req.body.userId
+                                                  }
+                                        }
+                              )
+
+                              res.status(200).json("The Post Has Been Unliked")
+                    }
+          } catch (err) {
+                    res.status(500).json(err)
+          }
+})
 
 // get a post
+router.get("/:id", async (req, res) => {
+          try {
+                    const post = await Post.findById(req.params.id)
+
+                    res.status(200).json(post)
+          } catch (err) {
+                    res.status(500).json(err)
+          }
+})
+
+// get all posts
+router.get("/", async (req, res) => {
+          try {
+                    const allPosts = await Post.find()
+
+                    res.status(200).json(allPosts)
+          } catch (err) {
+                    res.status(500).json(err)
+          }
+})
 
 
-// get timeline posts
+// get timeline (of pple u followed) posts
+router.get("/timeline/all", async (req, res) => {
 
+          try {
+                    const currentUser = await User.findById(req.body.userId)               
+                    const userPosts = await Post.find({userId: currentUser._id})
+
+                    const friendPosts = await Promise.all(
+                              currentUser.followings.map((friendId) => {
+                                        return Post.find({userId: friendId})
+                              })
+                    )
+
+                    // show both user and frds posts
+                    res.json(userPosts.concat(...friendPosts))
+          } catch (err) {
+                    res.status(500).json(err)
+          }
+})
 
 module.exports = router
