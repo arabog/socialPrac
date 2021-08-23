185d362a7f500ca353c61864d0c190f13bcecc51
iCode_Rx <arababs2015@gmail.com>
2021-08-23 10:57:43 +0100

    starting FS

--- clientPrac/src/App.js
+++ clientPrac/src/App.js
@@ -1,13 +1,42 @@
 import Home from "./pages/home/Home"
-// import Profile from "./pages/profile/Profile"
-// import Login from "./pages/login/Login"
-// import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile"
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	// Link
+} from "react-router-dom"
 
 function App() {
 	return (
-		<div>
-			<Home  />
-		</div>
+		<Router>
+
+			<Switch>
+
+				<Route exact to="/">
+					<Home />
+				</Route>
+
+
+				<Route to="/login">
+					<Login />
+				</Route>
+
+
+				<Route to="/register">
+					<Register />
+				</Route>
+
+
+				<Route to="/profile/:username">
+					<Profile />
+				</Route>
+
+			</Switch>
+
+		</Router>
 	);
 }
 
