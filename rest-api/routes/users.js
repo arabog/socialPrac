cccd20cb4a88c692dff1664d9876d0dfeb7a5c0c
iCode_Rx <arababs2015@gmail.com>
2021-08-24 10:40:12 +0100

    contextAPI created

--- rest-api/routes/users.js
+++ rest-api/routes/users.js
@@ -63,9 +63,14 @@ router.delete("/:id", async (req, res)=> {
 })
 
 // get a user
-router.get("/:id", async (req, res) => {
+router.get("/", async (req, res) => {
+          const userId = req.query.userId
+          const username = req.query.username
+
           try {
-                    const user = await User.findById(req.params.id)
+                    const user = userId 
+                                        ? await User.findById(userId)
+                                        : await User.findOne({username: username})
 
                     // hide vital info
                     const { password, updateAt, ...other} = user._doc
